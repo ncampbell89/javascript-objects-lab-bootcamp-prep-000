8330d7788cfdc4496357d34c1e95afa4b74c6240
Nicole Campbell <nscampbell303@gmail.com>
2017-10-03 20:29:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = new Object();
 
 updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({}, recipes, { [key]: value })
+  Object.assign({}, object, { [key]: value })
   return object
 }
