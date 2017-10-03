fd832e79462a2fb794801f03b4ae0257bd49f38a
Nicole Campbell <nscampbell303@gmail.com>
2017-10-03 20:54:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  return Object.assign({}, object, { delete[key]: value })
+  return Object.assign({}, object, delete { [key]: value })
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
